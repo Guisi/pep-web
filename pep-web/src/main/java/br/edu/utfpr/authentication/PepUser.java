7699d2ba8fac557f15c112f386212f2cfabe76c6
guisiagudos <guisiagudos@8c9a84a8-bf9d-47c2-83e8-7cdbc5a941eb>
2014-10-28 22:50:14 +0000

    Autenticação de usuário (parcial)

---
+++ pep-web/src/main/java/br/edu/utfpr/authentication/PepUser.java
@@ -0,0 +1,293 @@
+package br.edu.utfpr.authentication;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
+
+/**
+ * <p>
+ * Este usuario e utilizado para a autenticacao via Spring Security
+ * </p>
+ * 
+ * @author douglas.guisi
+ */
+public class PepUser implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+	private final String username;
+	private String credentials;
+	private final String nomeUsuario;
+	private final String documento;
+	private final String email;
+	private final boolean enabled;
+	private final boolean isCredentialsNonExpired;
+	private final Set<GrantedAuthority> autorizacoes;
+
+	/**
+	 * Construtor de OvdUser
+	 * 
+	 * @param username
+	 * @param nomeUsuario
+	 * @param email
+	 * @param documento
+	 * @param senhaExpirada
+	 * @param enabled
+	 * @param grupos
+	 * @param perfis
+	 * @param acessos
+	 * @param autorizacoes
+	 */
+	public PepUser(String username, String nomeUsuario, String email, String documento, boolean isCredentialsNonExpired, boolean enabled, Set<? extends GrantedAuthority> autorizacoes) {
+		this.username = username;
+		this.nomeUsuario = nomeUsuario;
+		this.email = email;
+		this.documento = documento;
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+		this.enabled = enabled;
+		this.autorizacoes = Collections.unmodifiableSet(sortAuthorities(autorizacoes));
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#getAuthorities()
+	 */
+	@Override
+	public Collection<GrantedAuthority> getAuthorities() {
+
+		return autorizacoes;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#getUsername()
+	 */
+	@Override
+	public String getUsername() {
+
+		return username;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#getPassword()
+	 */
+	@Override
+	public String getPassword() {
+
+		return credentials;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#isEnabled()
+	 */
+	@Override
+	public boolean isEnabled() {
+
+		return enabled;
+	}
+
+	/**
+	 * Retorna o nome do usuario
+	 * 
+	 * @return String
+	 */
+	public String getNomeUsuario() {
+
+		return nomeUsuario;
+	}
+
+	/**
+	 * Getter de email
+	 * 
+	 * @return o valor de email
+	 */
+	public String getEmail() {
+
+		return email;
+	}
+
+	/**
+	 * Getter de autorizacoes
+	 * 
+	 * @return o valor de autorizacoes
+	 */
+	public Set<GrantedAuthority> getAutorizacoes() {
+
+		return autorizacoes;
+	}
+
+	/**
+	 * Getter de credentials
+	 * 
+	 * @return o valor de credentials
+	 */
+	public String getCredentials() {
+
+		return credentials;
+	}
+
+	/**
+	 * Getter de documento
+	 * 
+	 * @return o valor de documento
+	 */
+	public String getDocumento() {
+
+		return documento;
+	}	
+	
+	/**
+	 * Faz a ordenacao das autorizacoes
+	 * 
+	 * @param authorities
+	 * @return SortedSet<GrantedAuthority>
+	 */
+	private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+
+		Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+		// Ensure array iteration order is predictable (as per UserDetails.getAuthorities() contract and SEC-717)
+		SortedSet<GrantedAuthority> sortedAuthorities = new TreeSet<GrantedAuthority>(new AuthorityComparator());
+
+		for (GrantedAuthority grantedAuthority : authorities) {
+			Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+			sortedAuthorities.add(grantedAuthority);
+		}
+
+		return sortedAuthorities;
+	}
+
+	/**
+	 * Faz a comparacao das autorizacoes
+	 * 
+	 * @author mauriciovigolo
+	 */
+	private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+
+			// Neither should ever be null as each entry is checked before adding it to the set.
+			// If the authority is null, it is a custom authority and should precede others.
+			if (g2.getAuthority() == null) {
+				return -1;
+			}
+
+			if (g1.getAuthority() == null) {
+				return 1;
+			}
+
+			return g1.getAuthority().compareTo(g2.getAuthority());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#isAccountNonExpired()
+	 */
+	@Override
+	public boolean isAccountNonExpired() {
+
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#isAccountNonLocked()
+	 */
+	@Override
+	public boolean isAccountNonLocked() {
+
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.springframework.security.core.userdetails.UserDetails#isCredentialsNonExpired()
+	 */
+	@Override
+	public boolean isCredentialsNonExpired() {
+
+		return isCredentialsNonExpired;
+	}
+	
+	/**
+	 * Retorna true se os usernames forem iguais, pois campos login e UNIQUE.
+	 * <p>
+	 * 
+	 * @param user
+	 * @return boolean
+	 */
+	@Override
+	public boolean equals(Object user) {
+
+		if (user instanceof PepUser) {
+			return username.equals(((PepUser) user).username);
+		}
+		return false;
+	}
+	
+	/**
+	 * Retorna o hashcode de username
+	 * <p>
+	 * 
+	 * @return int
+	 */
+	@Override
+	public int hashCode() {
+
+		return username.hashCode();
+	}
+
+	/**
+	 * Retorna as informacoes do usuario
+	 * <p>
+	 * 
+	 * @return String
+	 */
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.toString()).append(": ");
+		sb.append("Username: ").append(this.username).append("; ");
+		sb.append("Password: [PROTECTED]; ");
+		sb.append("Enabled: ").append(this.enabled).append("; ");
+
+		if (!autorizacoes.isEmpty()) {
+			sb.append("Granted Authorities: ");
+
+			boolean first = true;
+			for (GrantedAuthority auth : autorizacoes) {
+				if (!first) {
+					sb.append(",");
+				}
+				first = false;
+
+				sb.append(auth);
+			}
+		} else {
+			sb.append("Not granted any authorities");
+		}
+
+		return sb.toString();
+	}
+
+}
---
+++ pep-web/src/main/java/br/edu/utfpr/mbean/login/LoginMBean.java
@@ -0,0 +1,109 @@
+package br.edu.utfpr.mbean.login;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.AuthenticationException;
+
+import br.edu.utfpr.mbean.BaseMBean;
+
+/**
+ * ManagedBean da pagina de login da aplicacao
+ * 
+ * @author douglas.guisi
+ */
+@ManagedBean
+@RequestScoped
+public class LoginMBean extends BaseMBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String login;
+	private String mensagem;
+
+	/**
+	 * Metodo a ser executado depois da construcao do objeto. Este tem a finalidade de remover a mensagem de erro da sessao para que o usuario nao veja o erro anterior.
+	 */
+	@PostConstruct
+	public void init() {
+		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+
+		// Recupera o ultimo login do usuario se na sessao
+		if (sessionMap.get("SPRING_SECURITY_LAST_USERNAME") != null) {
+			login = sessionMap.get("SPRING_SECURITY_LAST_USERNAME").toString();
+		}
+
+		// Trata se ocorreu uma excecao na ultima execucao
+		AuthenticationException ex = (AuthenticationException) sessionMap.get("SPRING_SECURITY_LAST_EXCEPTION");
+		if (ex != null) {
+
+			if (ex.getMessage() != null) {
+				addErrorMessage(ex.getMessage());
+			} else {
+				addErrorMessage(getMsgs().getString("login.error.usuariosenhainvalido"));
+			}
+			HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+			session.invalidate();
+		}
+	}
+
+	/**
+	 * Faz a limpeza dos messages
+	 */
+	public void limparMessages() {
+		for (Iterator<FacesMessage> iterator = FacesContext.getCurrentInstance().getMessageList().iterator(); iterator.hasNext();) {
+			iterator.remove();
+		}
+		
+	}
+	
+	/**
+	 * Processar login do usuario.
+	 * 
+	 * @return
+	 */
+	public void processLogin() {
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		
+		try {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/j_spring_security_check");
+			dispatcher.forward(request, response);
+			FacesContext.getCurrentInstance().responseComplete();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	
+}
