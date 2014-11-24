83a634da6bdd3f08ea64daf7fa937cc3a135a6ad
guisiagudos <guisiagudos@8c9a84a8-bf9d-47c2-83e8-7cdbc5a941eb>
2014-11-24 20:14:48 +0000

    Manutenção de especialidades.

--- trunk/pep-web/src/main/java/br/edu/utfpr/dao/EspecialidadeDao.java
+++ trunk/pep-web/src/main/java/br/edu/utfpr/dao/EspecialidadeDao.java
@@ -3,6 +3,10 @@ package br.edu.utfpr.dao;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import br.edu.utfpr.model.Especialidade;
 import br.edu.utfpr.model.Especialidade_;
 
@@ -12,5 +16,14 @@ public class EspecialidadeDao extends GenericDao<Especialidade, Long> implements
 
 	public List<Especialidade> retornarEspecialidades() {
 		return findAll(Especialidade_.descricao);
+	}
+	
+	public Especialidade retornarEspecialidadePorDescricao(String descricao) {
+		CriteriaBuilder qb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Especialidade> q = qb.createQuery(Especialidade.class);
+		Root<Especialidade> root = q.from(Especialidade.class);
+		q.where(qb.equal(root.get(Especialidade_.descricao), descricao));
+		
+		return entityManager.createQuery(q).getSingleResult();
 	}
 }
--- trunk/pep-web/src/main/java/br/edu/utfpr/service/EspecialidadeService.java
+++ trunk/pep-web/src/main/java/br/edu/utfpr/service/EspecialidadeService.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.NoResultException;
+
+import org.apache.commons.lang.StringUtils;
 
 import br.edu.utfpr.dao.EspecialidadeDao;
 import br.edu.utfpr.exception.AppException;
@@ -19,6 +22,31 @@ public class EspecialidadeService {
 
 	public List<Especialidade> retornarEspecialidades() {
 		return especialidadeDao.retornarEspecialidades();
+	}
+	
+	public Especialidade retornarEspecialidade(Long id) {
+		return especialidadeDao.getById(id);
+	}
+	
+	public Especialidade retornarEspecialidadePorDescricao(String descricao) {
+		try {
+			return especialidadeDao.retornarEspecialidadePorDescricao(descricao);
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
+	
+	public void salvarEspecialidade(Especialidade especialidade) throws AppException {
+		String nomeEspecialidade = StringUtils.trimToNull(especialidade.getDescricao());
+		especialidade.setDescricao(nomeEspecialidade);
+		
+		//valida se nao esta duplicando
+		Especialidade especialidadeBase = this.retornarEspecialidadePorDescricao(nomeEspecialidade);
+		if (especialidadeBase != null && !especialidadeBase.getId().equals(especialidade.getId())) {
+			throw new AppException("especialidade.salvar.erro.descricaoexistente", nomeEspecialidade);
+		}
+		
+		especialidadeDao.save(especialidade);
 	}
 	
 	public void removerEspecialidade(Especialidade especialidade) throws AppException {
