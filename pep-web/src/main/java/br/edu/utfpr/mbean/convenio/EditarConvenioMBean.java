4af5e3974ac8041fb8f97f00392517d7f05c753d
guisiagudos <guisiagudos@8c9a84a8-bf9d-47c2-83e8-7cdbc5a941eb>
2014-11-25 10:46:43 +0000

    Tela de manutenção de convênios.

---
+++ pep-web/src/main/java/br/edu/utfpr/mbean/convenio/EditarConvenioMBean.java
@@ -0,0 +1,68 @@
+package br.edu.utfpr.mbean.convenio;
+
+import static javax.faces.context.FacesContext.getCurrentInstance;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang.StringUtils;
+
+import br.edu.utfpr.exception.AppException;
+import br.edu.utfpr.mbean.BaseMBean;
+import br.edu.utfpr.model.Convenio;
+import br.edu.utfpr.service.ConvenioService;
+
+@ManagedBean
+@ViewScoped
+public class EditarConvenioMBean extends BaseMBean {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ConvenioService convenioService;
+	
+	private Convenio convenioSelecionado;
+	
+	@PostConstruct
+	public void init() {
+		HttpServletRequest request = (HttpServletRequest) getCurrentInstance().getExternalContext().getRequest();
+		String idConvenio = request.getParameter("idConvenio");
+		
+		if (StringUtils.isNotEmpty(idConvenio) && StringUtils.isNumeric(idConvenio)) {
+			this.convenioSelecionado = convenioService.retornarConvenio(Long.parseLong(idConvenio));
+		} else {
+			this.convenioSelecionado = new Convenio();
+		}
+	}
+	
+	public String salvarConvenio() {
+		boolean isNew = convenioSelecionado.isNew();
+		try {
+			convenioService.salvarConvenio(convenioSelecionado);
+			convenioSelecionado = null;
+			
+			if (isNew) {
+				addInfoMessage(getMsgs().getString("convenio.criar.sucesso"), true);
+			} else {
+				addInfoMessage(getMsgs().getString("convenio.editar.sucesso"), true);
+			}
+			
+			return "/secure/convenio/convenios.xhtml?faces-redirect=true";
+		} catch (AppException e) {
+			addressException(e);
+		}
+		return null;
+	}
+
+	public Convenio getConvenioSelecionado() {
+		return convenioSelecionado;
+	}
+
+	public void setConvenioSelecionado(Convenio convenioSelecionado) {
+		this.convenioSelecionado = convenioSelecionado;
+	}
+
+}
