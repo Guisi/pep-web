c18279242a559765982e82d57ea2b6510553622d
guisiagudos <guisiagudos@8c9a84a8-bf9d-47c2-83e8-7cdbc5a941eb>
2015-05-29 19:46:24 +0000

    Antecedentes cirúrgicos no resumo do atendimento.

--- pep-web/src/main/resources/META-INF/import.sql
+++ pep-web/src/main/resources/META-INF/import.sql
@@ -289,11 +289,11 @@ ALTER SEQUENCE pep_owner.habito_sequence RESTART WITH 9;
 --#### atendimento #########
 --##########################
 --postgres
-INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (1, '2015-01-10 15:35', 3, 'História atendimento 1', 'ISDA atendimento 1');
-INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (2, '2015-05-12 10:30', 3, 'História atendimento 2', 'ISDA atendimento 2');
+--INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (1, '2015-01-10 15:35', 3, 'História atendimento 1', 'ISDA atendimento 1');
+--INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (2, '2015-05-12 10:30', 3, 'História atendimento 2', 'ISDA atendimento 2');
 --h2
---INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (1, PARSEDATETIME('10 Jan 2015 15:35 GMT',  'dd MMM yyyy HH:mm z', 'en', 'GMT'), 3, 'História atendimento 1', 'ISDA atendimento 1');
---INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (2, PARSEDATETIME('12 May 2015 10:30 GMT',  'dd MMM yyyy HH:mm z', 'en', 'GMT'), 3, 'História atendimento 2', 'ISDA atendimento 2');
+INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (1, PARSEDATETIME('10 Jan 2015 15:35 GMT',  'dd MMM yyyy HH:mm z', 'en', 'GMT'), 3, 'História atendimento 1', 'ISDA atendimento 1');
+INSERT INTO pep_owner.tb_atendimento(id_atendimento, data, id_paciente, historia_doenca_atual, isda) VALUES (2, PARSEDATETIME('12 May 2015 10:30 GMT',  'dd MMM yyyy HH:mm z', 'en', 'GMT'), 3, 'História atendimento 2', 'ISDA atendimento 2');
 ALTER SEQUENCE pep_owner.atendimento_sequence RESTART WITH 3;
 
 --queixas principais
@@ -320,8 +320,8 @@ ALTER SEQUENCE pep_owner.antecedente_clinico_atendimento_sequence RESTART WITH 5
 --antecedentes cirurgicos
 INSERT INTO pep_owner.tb_antecedente_cirurgico_atendimento(id_antecedente_cirurgico_atendimento, descricao, observacao, id_atendimento, id_procedimento) VALUES (1, null, 'Deu boa!', 1, 1);
 INSERT INTO pep_owner.tb_antecedente_cirurgico_atendimento(id_antecedente_cirurgico_atendimento, descricao, observacao, id_atendimento) VALUES (2, 'Facectomia', 'Nem sei o que é isso!', 1);
-INSERT INTO pep_owner.tb_antecedente_cirurgico_atendimento(id_antecedente_cirurgico_atendimento, descricao, observacao, id_atendimento) VALUES (3, 'Retinopexia', 'Muito menos isso...', 1);
-INSERT INTO pep_owner.tb_antecedente_cirurgico_atendimento(id_antecedente_cirurgico_atendimento, descricao, observacao, id_atendimento, id_procedimento) VALUES (4, null, 'Nem doeu!', 1, 2);
+INSERT INTO pep_owner.tb_antecedente_cirurgico_atendimento(id_antecedente_cirurgico_atendimento, descricao, observacao, id_atendimento) VALUES (3, 'Retinopexia', 'Muito menos isso...', 2);
+INSERT INTO pep_owner.tb_antecedente_cirurgico_atendimento(id_antecedente_cirurgico_atendimento, descricao, observacao, id_atendimento, id_procedimento) VALUES (4, null, 'Nem doeu!', 2, 2);
 ALTER SEQUENCE pep_owner.antecedente_cirurgico_atendimento_sequence RESTART WITH 5;
 
 --habitos
